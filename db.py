8f22e341cf140f014cbf90fea58fb3300814d5cd
Sycther <srflebbe@gmail.com>
2022-02-14 14:33:48 -0500

    Setup GraphCMS

---
+++ db.py
@@ -0,0 +1,40 @@
+from unittest import result
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+
+# Select your transport with a defined url endpoint
+transport = AIOHTTPTransport(url="https://api-us-east-1.graphcms.com/v2/ckzn0i50h55rt01yy2hsf9tdg/master")
+
+# Create a GraphQL client using the defined transport
+client = Client(transport=transport, fetch_schema_from_transport=True)
+
+queryBrothers = gql(
+    """
+    query BrotherList {
+        brothers {
+            name
+            title
+            desc
+            img {
+            url
+            }
+        }
+    }
+"""
+)
+
+queryEvents =gql(
+    """
+    query EventsList {
+        events{
+            title
+            desc
+            startTime
+            location
+            img {
+                url
+            }
+        }
+    }
+    """
+)
--- run.py
+++ run.py
@@ -2,43 +2,39 @@ import os
 import json, urllib
 
 from flask import Flask, render_template
-from butter_cms import ButterCMS
 
-# FIXME Fallback onto previously loaded file
+import db
 
-client = ButterCMS('857e1fbc8fccc396318e1c6bd4e023489dcc159f')
+def createApp() -> Flask:
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
-def loadJsonFile(url : str) -> str:
-    f = urllib.request.urlopen(url)
-    data = json.load(f)
-    return data
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-# create and configure the app
-application = Flask(__name__, instance_relative_config=True)
-application.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(application.instance_path, 'flaskr.sqlite'),
-)
+    # a simple page that says hello
+    @app.route('/', methods=['GET'])
+    def home():
+        return render_template("home.html")
 
-# ensure the instance folder exists
-try:
-    os.makedirs(application.instance_path)
-except OSError:
-    pass
+    @app.route('/brothers')
+    def our_brothers():
+        return render_template("brothers.html", content=db.client.execute(db.queryBrothers))
 
-# a simple page that says hello
-@application.route('/', methods=['GET'])
-def home():
-    return render_template("home.html")
+    @app.route('/events')
+    def hello():
+        return render_template("events.html", content=db.client.execute(db.queryEvents))
 
-@application.route('/brothers')
-def our_brothers():
-    return render_template("brothers.html", content=loadJsonFile("https://drive.google.com/uc?id=1y1wkroIhPU56XrPR-_ePwlX-msqYpsOA"))
-
-@application.route('/events')
-def hello():
-    return render_template("events.html", content=loadJsonFile("https://drive.google.com/uc?id=154Nx1RkquXmvy_w0zJPsxOlvlQ9xofO3"))
+    return app
 
 if __name__ == "__main__":
+    application = createApp()
     application.debug = False
     application.run()
